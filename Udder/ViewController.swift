a0b76a14469024ea824cb02e9ca7140c52777d27
Francois Slabber <fgslabber@yahoo.com>
2022-09-20 19:04:59 -0400

    Delete unnecessary comments and viewDidAppear.

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -11,12 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        //
-    }
 
 
 }
